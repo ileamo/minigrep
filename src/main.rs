d422f60e3dc7782fce1152940ae12e1202cf5284
imosunov <imosunov@nsg.net.ru>
2019-12-14 17:50:36 +0300

    Обработка ошибок

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Проблема с аргументами: {}", err);
+        process::exit(1);
+    });
+    println!("Поиск {}\nВ файле {}", config.query, config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Ошибка чтения файла");
+    println!("Текст:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Недостаточно аргументов");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
